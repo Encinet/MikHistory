067f045c91df8906b85ee5f4ca7a3b0d50d312ae
iamyuuk <2704804982@qq.com>
2025-02-13 17:03:21 +0800

    create README.md

--- convert.py
+++ convert.py
@@ -7,8 +7,14 @@ md_file = "MikHistory.md"
 with open(md_file, "r", encoding='utf-8') as f:
     markdown_text = f.read()
 
-html = markdown2.markdown(markdown_text)
 
+# 将MikHistory.md的内容复制到README.md
+readme_md_file = "README.md"
+with open(readme_md_file, "w", encoding="utf-8") as f:
+    f.write(markdown_text)
+
+# MD转换HTML
+html = markdown2.markdown(markdown_text)
 html_file = "index.html"
 with open(html_file, "w", encoding='utf-8') as f:
     f.write(f'''
